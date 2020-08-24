0db37932642ce45d76272487df1de7e95d69881e
Arcturion <dimascours@gmail.com>
2020-08-24 17:06:17 +0700

    Update auto.py
    lupa ngehapus API code

--- auto.py
+++ auto.py
@@ -7,7 +7,7 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboar
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.namedtuple import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent
 
-bot = telepot.Bot('1355362257:AAElq_EJOygVq4TlGC6d5pHVrhob768G4Ds')
+bot = telepot.Bot('rahasia')
 
 radar = '/root/data/sepuluh/current/citra_radar.png'
 
